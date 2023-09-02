17da8d5d758a1387368485e525c5b77b803bb26a
DAHYUN00 <kimdahyun000@inha.edu>
2023-09-02 17:27:45 +0900

    1주차

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '알리익스프레스' });
+});
+
+module.exports = router;
